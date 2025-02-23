c2bd8974d2d6631a7d92274306eee7b5efe077c4
mariakalafati <t8190051@aueb.gr>
2025-02-23 22:33:46 +0200

    Added Basic Controllers

--- Controllers/WishlistController.cs
+++ Controllers/WishlistController.cs
@@ -3,7 +3,6 @@ using WishlistExpress.Data;
 
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Runtime.CompilerServices;
 
 namespace WishlistExpress.Controllers;
 
