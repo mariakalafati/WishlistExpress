191f4bd9f7a6230d1ee7dedebbb422c3188955bf
mariakalafati <t8190051@aueb.gr>
2025-02-23 22:26:54 +0200

    Added Basic Controllers

---
+++ Controllers/SubmissionController.cs
@@ -0,0 +1,49 @@
+using WishlistExpress.Models;
+using WishlistExpress.Data;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+
+namespace WishlistExpress.Controllers;
+
+public class SubmissionController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public SubmissionController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index(int wishlistId, int userId)
+    {
+        var submissions = await _context.Submissions
+            .Where(s => s.WishlistId == wishlistId && s.UserId == userId)
+            .ToListAsync();
+        return View(submissions);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Submission submission)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Submissions.Add(submission);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Success");
+        }
+        return View(submission);
+    }
+
+    public IActionResult Success()
+    {
+        return View();
+    }
+}
---
+++ Controllers/WishlistController.cs
@@ -0,0 +1,42 @@
+using WishlistExpress.Models;
+using WishlistExpress.Data;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+
+namespace WishlistExpress.Controllers;
+
+public class WishlistController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public WishlistController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index(int userId)
+    {
+        var wishlists = await _context.Wishlists.Where(w => w.UserId == userId).ToListAsync();
+        return View(wishlists);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Wishlist wishlist)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Wishlists.Add(wishlist);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", new { userId = wishlist.UserId });
+        }
+        return View(wishlist);
+    }
+}
